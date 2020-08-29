dac6701b75a7141d77d0f36867d07550c4857b5b
Adam Múdry <mudry.ado@gmail.com>
2020-08-29 03:28:11 +0200

    page update

--- src/cv.nim
+++ src/cv.nim
@@ -3,24 +3,46 @@ import jsutils, dom, jsffi
 
 type
   ActionType {.pure.} = enum
-    NavbarAction, ButtonAction
+    NavbarAction, ButtonAction, LanguageAction
+
+  Language {.pure.} = enum
+    English, Slovak
 
   ContentItem = object
-    title, content : kstring
+    title, content : array[2, kstring]
 
 const
-  text_about : kstring = """
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in pulvinar dolor. Fusce non porta est. Donec blandit feugiat leo, ut porta urna. Fusce est augue, tempor ac risus eu, consectetur aliquam nisl. Nulla consectetur, purus consectetur tempor ultricies, leo lectus commodo ante, at varius erat ex ac tortor. Aenean scelerisque nisl in blandit volutpat. Donec nec vehicula nisi, et feugiat quam. Mauris nec nisi sit amet purus interdum fermentum. Aenean rutrum condimentum dapibus. Morbi vulputate ut lorem quis iaculis. Donec pharetra velit ac convallis ornare.
+  languages = [kstring"EN", "SK"]
 
-Duis scelerisque, enim vitae sollicitudin semper, ante odio tempus mauris, non porttitor ante justo et metus. Praesent lacinia consectetur consequat. Integer iaculis, urna quis ullamcorper maximus, urna tellus dignissim elit, eget luctus diam orci in lectus. Praesent tristique vehicula justo vitae pulvinar. Phasellus tincidunt, orci ut suscipit tincidunt, augue massa laoreet metus, ut mollis purus purus nec enim. Cras vitae pharetra lacus. Integer volutpat, nisi quis tempor ornare, tellus sapien ultricies odio, in condimentum purus risus in ante. Aliquam erat volutpat. Aliquam sed nibh feugiat, tempus elit at, pulvinar arcu. Aliquam porta mi sodales, rhoncus eros vitae, venenatis lectus. Ut interdum nisi quis ante gravida lacinia. Nunc pretium, odio non molestie ultrices, libero mi gravida orci, quis lacinia leo elit ac arcu. Integer pretium elementum odio sed commodo. Quisque eget ligula a lectus pharetra sagittis id non elit. Nullam vitae consequat libero.
+  about_title = [kstring"About", "O mne"]
+  about_text = [kstring"""
+About me in English...
+""",
 """
-  text_skills : kstring = ""
-  text_projects : kstring = ""
+O mne po slovensky...
+"""]
 
-  navbar_list : seq[ContentItem] = @[ContentItem(title: "About", content: text_about),
-                                     ContentItem(title: "Skills", content: text_skills),
-                                     ContentItem(title: "Projects", content: text_projects)]
-var mode = 0
+  skills_title = [kstring"", ""]
+  skills_text = [kstring"", ""]
+
+  projects_title = [kstring"", ""]
+  projects_text = [kstring"", ""]
+
+  navbar_list : seq[ContentItem] = @[ContentItem(title: about_title, content: about_text),
+                                     ContentItem(title: skills_title, content: skills_text),
+                                     ContentItem(title: projects_title, content: projects_text)]
+var
+  shown_content = 0
+  language : Language = Slovak
+
+proc `[]`(obj: JsObject; field: Language): JsObject =
+  result = obj[ord(field)]
+
+proc `[]`[T](arr: openArray[T]; field: Language): T =
+  result = arr[ord(field)]
+
+proc `[]`[T](arr: openArray[var T]; field: Language): var T =
+  result = arr[ord(field)]
 
 proc action(typ: ActionType, entry: kstring): proc() =
   result = proc() = 
@@ -28,8 +50,15 @@ proc action(typ: ActionType, entry: kstring): proc() =
     of NavbarAction:
       echo "clicked \"", entry, "\" menu button"
       for i, item in navbar_list:
-        if entry == item.title:
-          mode = i
+        if entry == item.title[language]:
+          shown_content = i
+
+    of LanguageAction:
+      echo "clicked language button"
+      if language == English:
+        language = Slovak
+      elif language == Slovak:
+        language = English
 
     of ButtonAction:
       echo "clicked \"", entry, "\" normal button"
@@ -38,16 +67,18 @@ proc buildNavbar(): VNode =
   result = buildHtml(nav(class="navbar is-primary")):
     tdiv(class="navbar-list center"):
       for i, m in navbar_list:
-        if i != 0:
-          span():
-            text "/"
-        a(class="navbar-item", onclick=action(NavbarAction, m.title)):
-          text m.title
+        span():
+          text "/"
+        a(class="navbar-item", onclick=action(NavbarAction, m.title[language])):
+          text m.title[language]
+
+      a(class="navbar-item", onclick=action(LanguageAction, "")):
+          text languages[language]
 
 proc buildContent(): VNode =
   result = buildHtml(tdiv):
     tdiv(class="content center"):
-      text navbar_list[mode].content
+      text navbar_list[shown_content].content[language]
 
 proc createDom(): VNode =
   result = buildHtml(tdiv):
